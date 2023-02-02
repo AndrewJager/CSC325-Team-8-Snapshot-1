812030196c8cfa83575b5889c87f70c1c33961ea
AndrewJager <35353143+AndrewJager@users.noreply.github.com>
2023-02-02 13:51:13 -0500

    Create channels when class is created

--- commands/newclass.js
+++ commands/newclass.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, ChannelType } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -11,7 +11,39 @@ module.exports = {
 		const dept = interaction.options.getString('dept');
 		const course = interaction.options.getString('classcode');
 		const semester = interaction.options.getString('semester');
-		await interaction.reply({ content: 'user entered class ' + dept
+
+        interaction.guild.channels.create({
+            name: dept + ' ' + course + ' - ' + semester,
+            type: ChannelType.GuildCategory,
+        }).then(category => {
+            interaction.guild.channels.create({
+                name: 'announcements-' + course,
+                type: ChannelType.GuildText,
+                parent: category.id,
+            });
+            interaction.guild.channels.create({
+                name: 'zoom-meeting-info-' + course,
+                type: ChannelType.GuildText,
+                parent: category.id,
+            });
+            interaction.guild.channels.create({
+                name: 'how-to-make-a-video',
+                type: ChannelType.GuildText,
+                parent: category.id,
+            });
+            interaction.guild.channels.create({
+                name: 'introduce-yourself',
+                type: ChannelType.GuildText,
+                parent: category.id,
+            });
+            interaction.guild.channels.create({
+                name: 'chat',
+                type: ChannelType.GuildText,
+                parent: category.id,
+            });
+        });
+
+		await interaction.reply({ content: 'Created class ' + dept
                         + course + ' in semester ' + semester, fetchReply: true });
 	},
 };
