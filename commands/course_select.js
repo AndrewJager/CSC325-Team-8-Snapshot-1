637c1e000e20ccadac5dc54e604ae684d8d94303
nmcghee-png <nmcghee@umich.edu>
2023-02-02 13:41:12 -0500

    changed course_select
    welcome_select -> course_select
    command is still /courses
    
    requires 5 roles to be used

---
+++ commands/course_select.js
@@ -0,0 +1,89 @@
+const { SlashCommandBuilder, EmbedBuilder, Embed, PermissionsBitField, ButtonStyle, ActionRowBuilder, ButtonBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('courses')
+		.setDescription('assign courses to yourself')
+		.addRoleOption(option => option.setName('role1').setDescription('First role you want to see set up').setRequired(true))
+		.addRoleOption(option => option.setName(`role2`).setDescription(`Second role you want to see set up`).setRequired(true))
+		.addRoleOption(option => option.setName(`role3`).setDescription(`Third role you want to see set up`).setRequired(true))
+		.addRoleOption(option => option.setName(`role4`).setDescription(`Third role you want to see set up`).setRequired(true))
+		.addRoleOption(option => option.setName(`role5`).setDescription(`Third role you want to see set up`).setRequired(true)),
+
+	async execute(interaction, client) {
+
+		const role1 = interaction.options.getRole('role1');
+		const role2 = interaction.options.getRole(`role2`);
+		const role3 = interaction.options.getRole(`role3`);
+		const role4 = interaction.options.getRole(`role4`);
+		const role5 = interaction.options.getRole(`role5`);
+
+		const button = new ActionRowBuilder()
+			.addComponents(
+				new ButtonBuilder()
+				.setCustomId('button1')
+				.setLabel(`${role1.name}`)
+				.setStyle(ButtonStyle.Secondary),
+
+                new ButtonBuilder()
+                .setCustomId(`button2`)
+                .setLabel(`${role2.name}`)
+                .setStyle(ButtonStyle.Secondary),
+
+                new ButtonBuilder()
+                .setCustomId(`button3`)
+                .setLabel(`${role3.name}`)
+                .setStyle(ButtonStyle.Secondary),
+                
+                new ButtonBuilder()
+                .setCustomId(`button4`)
+                .setLabel(`${role4.name}`)
+                .setStyle(ButtonStyle.Secondary),
+                
+                new ButtonBuilder()
+                .setCustomId(`button5`)
+                .setLabel(`${role5.name}`)
+                .setStyle(ButtonStyle.Secondary),
+            
+			);
+
+		const embed = new EmbedBuilder()
+			.setTitle('Course Selection.')
+            .setDescription('Select a course you are registered to.')
+			.setColor('Black');
+
+		await interaction.reply({ embeds: [embed], components: [button] });
+
+		const collector = await interaction.channel.createMessageComponentCollector();
+
+		collector.on('collect', async (i) => {
+			const member = i.member;
+
+			if (i.guild.members.me.roles.highest.position < role1.position) {
+				i.update({ content: 'My role is below the roles I am trying to mess with.', ephemeral:true });
+				return;
+			}
+
+			if (i.customId === 'button1') {
+				member.roles.add(role1);
+				i.reply({ content: 'Role added', ephemeral:true });
+			}
+			else if (i.customId === 'button2') {
+				member.roles.add(role2);
+				i.reply({ content: 'Role added', ephemeral:true });
+			}else if (i.customId === 'button3') {
+				member.roles.add(role3);
+				i.reply({ content: 'Role added', ephemeral:true });
+			}else if (i.customId === 'button4') {
+				member.roles.add(role4);
+				i.reply({ content: 'Role added', ephemeral:true });
+			}else if (i.customId === 'button5') {
+				member.roles.add(role5);
+				i.reply({ content: 'Role added', ephemeral:true });
+			}
+
+		});
+	},
+
+
+};
