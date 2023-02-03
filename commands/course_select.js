6dce301bb5cac8df71e8e4dd9db5b138e732c99b
nmcghee-png <nmcghee@umich.edu>
2023-02-02 20:02:29 -0500

    welcome and course_select
    added more to the welcome message and course_select

--- commands/course_select.js
+++ commands/course_select.js
@@ -1,14 +1,15 @@
-const { SlashCommandBuilder, EmbedBuilder, Embed, PermissionsBitField, ButtonStyle, ActionRowBuilder, ButtonBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, Embed, PermissionsBitField, ButtonStyle, ActionRowBuilder, ButtonBuilder, ActionRow, TeamMemberMembershipState } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('courses')
 		.setDescription('assign courses to yourself')
-		.addRoleOption(option => option.setName('role1').setDescription('First role you want to see set up').setRequired(true))
-		.addRoleOption(option => option.setName(`role2`).setDescription(`Second role you want to see set up`).setRequired(true))
-		.addRoleOption(option => option.setName(`role3`).setDescription(`Third role you want to see set up`).setRequired(true))
-		.addRoleOption(option => option.setName(`role4`).setDescription(`Third role you want to see set up`).setRequired(true))
-		.addRoleOption(option => option.setName(`role5`).setDescription(`Third role you want to see set up`).setRequired(true)),
+		.addRoleOption(option => option.setName('role1').setDescription('1st role you want to add.').setRequired(true))
+		.addRoleOption(option => option.setName(`role2`).setDescription('2nd role you want to add.').setRequired(true))
+		.addRoleOption(option => option.setName(`role3`).setDescription('3rd role you want to add.').setRequired(true))
+		.addRoleOption(option => option.setName(`role4`).setDescription('4th role you want to add.').setRequired(true))
+		.addRoleOption(option => option.setName(`role5`).setDescription('5th role you want to add.').setRequired(true))
+		.addRoleOption(option => option.setName(`role6`).setDescription('6th role you want to add.').setRequired(true)),
 
 	async execute(interaction, client) {
 
@@ -17,6 +18,8 @@ module.exports = {
 		const role3 = interaction.options.getRole(`role3`);
 		const role4 = interaction.options.getRole(`role4`);
 		const role5 = interaction.options.getRole(`role5`);
+		const role6 = interaction.options.getRole(`role6`);
+		
 
 		const button = new ActionRowBuilder()
 			.addComponents(
@@ -25,7 +28,7 @@ module.exports = {
 				.setLabel(`${role1.name}`)
 				.setStyle(ButtonStyle.Secondary),
 
-                new ButtonBuilder()
+            	 new ButtonBuilder()
                 .setCustomId(`button2`)
                 .setLabel(`${role2.name}`)
                 .setStyle(ButtonStyle.Secondary),
@@ -43,15 +46,39 @@ module.exports = {
                 new ButtonBuilder()
                 .setCustomId(`button5`)
                 .setLabel(`${role5.name}`)
-                .setStyle(ButtonStyle.Secondary),
+                .setStyle(ButtonStyle.Secondary), 
             
 			);
+		/* const button2 = new ActionRowBuilder()
+				.addComponents (
+					new ButtonBuilder()
+					.setCustomId('button6')
+					.setLabel(`${role6.name}`)
+					.setStyle(ButtonStyle.Secondary),
+				) */
 
 		const embed = new EmbedBuilder()
-			.setTitle('Course Selection.')
-            .setDescription('Select a course you are registered to.')
-			.setColor('Black');
-
+			.setTitle('Course Selection Tutorial')
+            .setDescription('Read the steps carefully to ensure you that you get into the right class that you are registered for.')
+			.setColor('Yellow')
+			.addFields([
+				{
+					name: 'Step 1',
+					value: 'Verify what classes you have with Professor Spradling in SIS or your Class Schedule.',
+				},
+				{
+					name: 'Step 2',
+					value: 'View the courses below and verify that the ones you need match the ones you are registered in SIS with.',
+				},
+				{
+					name: 'Step 3',
+					value: 'Click the button that is labelled after the class that you need. You will receive a message that will say that the role was successfully added.',
+				},
+				{
+					name: 'Step 4',
+					value: 'Verify that the channels you need access to were added to the sidebar after selecting the classes you need. You will see the left sidebar populate with channels after you click the buttons that are the classes you need.',
+				},
+			])
 		await interaction.reply({ embeds: [embed], components: [button] });
 
 		const collector = await interaction.channel.createMessageComponentCollector();
@@ -59,16 +86,10 @@ module.exports = {
 		collector.on('collect', async (i) => {
 			const member = i.member;
 
-			if (i.guild.members.me.roles.highest.position < role1.position) {
-				i.update({ content: 'My role is below the roles I am trying to mess with.', ephemeral:true });
-				return;
-			}
-
 			if (i.customId === 'button1') {
 				member.roles.add(role1);
 				i.reply({ content: 'Role added', ephemeral:true });
-			}
-			else if (i.customId === 'button2') {
+			}else if (i.customId === 'button2') {
 				member.roles.add(role2);
 				i.reply({ content: 'Role added', ephemeral:true });
 			}else if (i.customId === 'button3') {
@@ -80,6 +101,12 @@ module.exports = {
 			}else if (i.customId === 'button5') {
 				member.roles.add(role5);
 				i.reply({ content: 'Role added', ephemeral:true });
+			}else if (i.customId === 'button6') {
+				member.roles.add(role6);
+				i.reply({ content: 'Role added', ephemeral:true });
+			}
+			else {
+				i.reply({ content: 'nothing happened', ephemeral:true });
 			}
 
 		});
